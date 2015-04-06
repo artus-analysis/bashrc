30932be45eda4f372375450dc6ecb30669620ee8
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-04-06 10:27:23 +0200

    ro.py: like rot.py but slimmer

---
+++ scripts/ro.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+"""
+from https://root.cern.ch/phpBB3/viewtopic.php?f=14&t=12521
+
+like rot.py, but
+	- python instead of ipython
+	- no print __doc__
+	- no loading of kappa lib
+	- no terminal output
+
+
+"""
+
+import os, sys, time
+
+def main():
+	os.close(0)  # close stdin BEFORE importing ROOT
+	import ROOT
+
+	infiles=[ROOT.TFile.Open(name) for name in sys.argv[1:]]
+	tbrowse=ROOT.TBrowser()
+
+	try:
+		while tbrowse:
+			time.sleep(1)
+	except KeyboardInterrupt,e:
+		pass
+
+if __name__ == "__main__":
+	try:
+		sys.exit(main())
+	except SystemExit:
+		pass
