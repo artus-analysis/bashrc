04850a785031e2e052a831d5b1f7e06d3509946b
Georg Fleig <gfleig@ekp.uni-karlsruhe.de>
2015-04-16 10:00:35 +0200

    gfleig.sh: updated

--- users/gfleig.sh
+++ users/gfleig.sh
@@ -13,7 +13,6 @@ export LANG=en_US.UTF-8
 export EDITOR=nano
 export LS_OPTIONS='-N -T 0 --color=auto'
 export PYTHONSTARTUP=$HOME/.pythonrc
-export KRB5CCNAME=$HOME/krb
 
 
 ####################################################################
@@ -44,7 +43,7 @@ mini() # user programs at ekp
 				eval `scramv1 runtime -sh`
 				cd /afs/desy.de/user/g/gfleig/zjet/Excalibur
 				source scripts/ini_excalibur.sh
-				export EXCALIBUR_WORK=/nfs/dust/cms/user/gfleig/excalibur
+				export EXCALIBUR_WORK=/nfs/dust/cms/user/gfleig/zjet
 				export PATH=$PATH:/afs/desy.de/user/g/gfleig/zjet/grid-control
 			else
 				cd /home/gfleig/new/CMSSW_7_2_0/src
