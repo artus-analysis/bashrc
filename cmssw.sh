7a2afde6e8de7ca0791226f7576ad4c80feefe02
Thomas Müller <t.muller@cern.ch>
2016-04-04 13:49:48 +0200

    cmssw.sh: small improvements

--- cmssw.sh
+++ cmssw.sh
@@ -3,14 +3,17 @@
 #source cmssw
 set_cmssw()
 {
-    export SCRAM_ARCH=${1-:slc5_amd64_gcc462}
-    if [ -z "$VO_CMS_SW_DIR" ]
-    then
-        echo "ERROR: 'VO_CMS_SW_DIR' is not set!"
-    else
-        source $VO_CMS_SW_DIR/cmsset_default.sh
-        eval `scram runtime -sh`
-    fi
+	export SCRAM_ARCH=${1-:slc5_amd64_gcc462}
+	if [ -z "$VO_CMS_SW_DIR" ]
+	then
+		echo "ERROR: 'VO_CMS_SW_DIR' is not set!"
+	else
+		source $VO_CMS_SW_DIR/cmsset_default.sh
+		if [ ! -z "$CMSSW_BASE" ]
+		then
+			eval `scramv1 runtime -sh`
+		fi
+	fi
 }
 
 alias cmssw_slc5_gcc462="set_cmssw slc5_amd64_gcc462"
