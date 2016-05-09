d18d70765e304274854fb1c555c353ea757e95ad
Thomas Mueller <t.muller@cern.ch>
2016-05-09 14:13:46 +0200

    tmuller.sh: update

--- users/tmuller.sh
+++ users/tmuller.sh
@@ -15,11 +15,22 @@ export EDITOR=vim
 export LS_OPTIONS="-N -T 0 --color=auto"
 
 # ALIASES
-alias scramb="scram b -j 8; echo $?"
-alias myrsync="rsync -avSzh --progress"
-alias myhtop="htop -u $USER"
-alias gmerge="(export PATH=/usr/bin/:$PATH && git mergetool --tool meld)"
-alias myvomsproxyinit="voms-proxy-init --voms cms:/cms/dcms --valid 192:00"
+alias scramb='scram b -j 8; echo $?'
+alias myrsync='rsync -avSzh --progress'
+alias myhtop='htop -u $USER'
+alias gmerge='(export PATH=/usr/bin/:$PATH && git mergetool --tool meld)'
+alias myvomsproxyinit='voms-proxy-init --voms cms:/cms/dcms --valid 192:00'
+
+# CMSSW
+alias setkitanalysis='setkitanalysis715'
+alias setkitskimming='setkitskimming763'
+alias setgenerator='setgenerator71162'
+
+# dCache
+# https://twiki.opensciencegrid.org/bin/view/ReleaseDocumentation/LcgUtilities#Using_LCG_Utils_commands
+alias mylcg-ls='lcg-ls -b -v -l -D srmv2'
+alias mylcg-cp='lcg-cp -v -b -D srmv2'
+alias mylcg-del='lcg-del -b -v -l -D srmv2'
 
 # Syntax highlighting in less
 VLESS=$(find /usr/share/vim -name "less.sh")
@@ -39,25 +50,35 @@ setgitcolors()
 
 # grid-control
 
-# dCache
-# https://twiki.opensciencegrid.org/bin/view/ReleaseDocumentation/LcgUtilities#Using_LCG_Utils_commands
-alias mylcg-ls="lcg-ls -b -v -l -D srmv2"
-alias mylcg-cp="lcg-cp -v -b -D srmv2"
-alias mylcg-del="lcg-del -b -v -l -D srmv2"
-
 
 # Artus
-export USERPC="lx3b28"
+export USERPC='lx3b28'
 
 
 setkitanalysis715() {
-	#setgridcontroltrunk1501
-	
 	cd ~/home/cms/htt/analysis/CMSSW_7_1_5/src
 	
 	set_cmssw slc6_amd64_gcc481
 	
 	source $CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/scripts/ini_KITHiggsToTauTauAnalysis.sh
+	
+	cd $CMSSW_BASE/src/
+}
+
+
+setkitskimming763() {
+	cd ~/home/cms/htt/skimming/CMSSW_7_6_3/src
+	
+	set_cmssw slc6_amd64_gcc493
+	
+	cd $CMSSW_BASE/src/
+}
+
+
+setgenerator71162() {
+	cd ~/home/cms/htt/generator/CMSSW_7_1_16_patch2/src
+	
+	set_cmssw slc6_amd64_gcc481
 	
 	cd $CMSSW_BASE/src/
 }
