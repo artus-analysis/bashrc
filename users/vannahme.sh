8cbf3964332b6b07e2cb9022bc1de6baadb4cadc
Kai Simon Vannahme <vannahme@lx3b09.physik.rwth-aachen.de>
2017-03-10 16:21:44 +0100

    newest Version

--- users/vannahme.sh
+++ users/vannahme.sh
@@ -27,8 +27,6 @@ alias gitpullcmssw='git fetch origin && git merge origin/master'
 
 # CMSSW
 alias setkitanalysis='setkitanalysis747'
-alias setkitskimming='setkitskimming8021'
-alias settauvalidation='settauvalidation9001'
 alias setcrab='setcrab3'
 export PATH=/afs/cern.ch/user/v/valya/public/dasgoclient/:$PATH
 
@@ -64,90 +62,13 @@ setcrab3() {
 export USERPC='lx3b85'
 
 
-setkitanalysis715() {
-	cd ~/home/cms/htt/analysis/CMSSW_7_1_5/src
-	
-	set_cmssw slc6_amd64_gcc481
-	
-	source $CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/scripts/ini_KITHiggsToTauTauAnalysis.sh
-	
-	cd $CMSSW_BASE/src/
-}
 setkitanalysis747() {
-	cd ~/home/cms/htt/analysis/CMSSW_7_4_7/src
+	cd /net/scratch_cms3b/kvannahme/analysis/CMSSW_7_4_7/src
 	
 	set_cmssw slc6_amd64_gcc491
 	
 	source $CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/scripts/ini_KITHiggsToTauTauAnalysis.sh
 	
 	cd $CMSSW_BASE/src/
-}
-
-
-setkitskimming763() {
-	cd ~/home/cms/htt/skimming/CMSSW_7_6_3/src
-	
-	set_cmssw slc6_amd64_gcc493
-	
-	cd $CMSSW_BASE/src/
-}
-setkitskimming8010() {
-	cd ~/home/cms/htt/skimming/CMSSW_8_0_10/src
-	
-	set_cmssw slc6_amd64_gcc530
-	
-	cd $CMSSW_BASE/src/
-}
-setkitskimming8021() {
-	cd ~/home/cms/htt/skimming/CMSSW_8_0_21/src
-	
-	set_cmssw slc6_amd64_gcc530
-	
-	cd $CMSSW_BASE/src/
-}
-
-
-setgenerator() {
-	cd ~/home/cms/htt/generator/
-	set_cmssw slc6_amd64_gcc481
-}
-
-
-setgenerator71162() {
-	setgenerator
-	cd CMSSW_7_1_16_patch2/src
-	
-	set_cmssw slc6_amd64_gcc481
-	
-	cd $CMSSW_BASE/src/
-}
-setgenerator7118() {
-	setgenerator
-	cd CMSSW_7_1_18/src
-	
-	set_cmssw slc6_amd64_gcc481
-	
-	cd $CMSSW_BASE/src/
-}
-
-settauvalidation9001() {
-	cd ~/home/cms/htt/tauvalidation/CMSSW_9_0_0_pre1/src
-	
-	set_cmssw slc6_amd64_gcc530
-	
-	cd $CMSSW_BASE/src/
-}
-
-
-setjuno() {
-	[ ! -e ~/bin ] && mkdir ~/bin
-	[ ! -e ~/bin/python ] && ln -s `which python27` ~/bin/python
-	export PATH=~/bin:$PATH
-
-	cd ~/home/juno/
-	source Artus/Configuration/scripts/ini_artus_python_standalone.sh
-	source Artus/HarryPlotter/scripts/ini_harry.sh
-
-	cd ReliabilityCalc
 }
 
