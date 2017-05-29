a7c749803fd9eb753c6227807131e469b8cd9cb0
Cristian Ramon Adam <adam@lx3b15.physik.rwth-aachen.de>
2017-05-29 17:52:56 +0200

    first commit

--- users/adam.sh
+++ users/adam.sh
@@ -35,9 +35,6 @@ alias settauvalidation='settauvalidation9001'
 alias setcrab='setcrab3'
 export PATH=/afs/cern.ch/user/v/valya/public/dasgoclient/:$PATH
 
-# Operation
-alias rdesktop_cerntscms='rdesktop -g 90% -a24 -u tmuller -d CERN cerntscms.cern.ch'
-
 # dCache
 # https://twiki.opensciencegrid.org/bin/view/ReleaseDocumentation/LcgUtilities#Using_LCG_Utils_commands
 alias mylcg-ls='lcg-ls -b -v -l -D srmv2'
@@ -70,96 +67,13 @@ setcrab3() {
 export USERPC='lx3b85'
 
 
-setkitanalysis715() {
-	cd ~/home/cms/htt/analysis/CMSSW_7_1_5/src
-	
-	set_cmssw slc6_amd64_gcc481
-	
-	source $CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/scripts/ini_KITHiggsToTauTauAnalysis.sh
-	
-	cd $CMSSW_BASE/src/
-}
+
 setkitanalysis747() {
-	cd ~/home/cms/htt/analysis/CMSSW_7_4_7/src
+	cd /.automount/net_rw/net__scratch_cms3b/adam/adam/CMSSW_7_4_7/src
 	
 	set_cmssw slc6_amd64_gcc491
 	
 	source $CMSSW_BASE/src/HiggsAnalysis/KITHiggsToTauTau/scripts/ini_KITHiggsToTauTauAnalysis.sh
 	
 	cd $CMSSW_BASE/src/
-}
-
-
-setkitskimming763() {
-	cd ~/home/cms/htt/skimming/CMSSW_7_6_3/src
-	
-	set_cmssw slc6_amd64_gcc493
-	
-	cd $CMSSW_BASE/src/
-}
-setkitskimming8010() {
-	cd ~/home/cms/htt/skimming/CMSSW_8_0_10/src
-	
-	set_cmssw slc6_amd64_gcc530
-	
-	cd $CMSSW_BASE/src/
-}
-setkitskimming8021() {
-	cd ~/home/cms/htt/skimming/CMSSW_8_0_21/src
-	
-	set_cmssw slc6_amd64_gcc530
-	
-	cd $CMSSW_BASE/src/
-}
-setkitskimming80261() {
-	cd ~/home/cms/htt/skimming/CMSSW_8_0_26_patch1/src
-	
-	set_cmssw slc6_amd64_gcc530
-	
-	cd $CMSSW_BASE/src/
-}
-
-
-setgenerator() {
-	cd ~/home/cms/htt/generator/
-	set_cmssw slc6_amd64_gcc481
-}
-
-
-setgenerator71162() {
-	setgenerator
-	cd CMSSW_7_1_16_patch2/src
-	
-	set_cmssw slc6_amd64_gcc481
-	
-	cd $CMSSW_BASE/src/
-}
-setgenerator7118() {
-	setgenerator
-	cd CMSSW_7_1_18/src
-	
-	set_cmssw slc6_amd64_gcc481
-	
-	cd $CMSSW_BASE/src/
-}
-
-settauvalidation9001() {
-	cd ~/home/cms/htt/tauvalidation/CMSSW_9_0_0_pre1/src
-	
-	set_cmssw slc6_amd64_gcc530
-	
-	cd $CMSSW_BASE/src/
-}
-
-
-setjuno() {
-	[ ! -e ~/bin ] && mkdir ~/bin
-	[ ! -e ~/bin/python ] && ln -s `which python27` ~/bin/python
-	export PATH=~/bin:$PATH
-
-	cd ~/home/juno/
-	source Artus/Configuration/scripts/ini_artus_python_standalone.sh
-	source Artus/HarryPlotter/scripts/ini_harry.sh
-
-	cd ReliabilityCalc
 }
