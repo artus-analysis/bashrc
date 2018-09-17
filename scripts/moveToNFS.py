3e850fd617baf1149bf30f16a3c7811ce0f5f80c
Olena Hlushchenko <greyxray@gmail.com>
2018-09-17 15:39:28 +0200

    olena: movetoNaf

--- scripts/moveToNFS.py
+++ scripts/moveToNFS.py
@@ -9,8 +9,6 @@ import argparse
 """Script to copy files after the skim from * to nfs."""
 import os
 import subprocess
-import shlex
-import shutil
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -18,11 +16,102 @@ pp = pprint.PrettyPrinter(indent=4)
 class MoveToNFS(object):
     """Class to copy files after the skim from * to nfs."""
 
+    # ussed to store common location accesed per-account
+    # needed to estimate the sub-directory structure that has to be preserved
     common_locations = {
+        # "to_private": "to_private_placeholder",
         "nafnfs": "/nfs/dust/cms/user/",
-        "nafnfs-olena": "/nfs/dust/cms/user/glusheno/afs/",
+        "nafnfs_zeus": "/nfs/dust/zeus/group/",
     }
 
+    def expandPath(self, *argv):
+        print "argv:", argv
+        if len(argv) == 0:
+            print "Trying to expand None"
+            exit(1)
+        expanded_list = []
+        for i in range(0, len(argv)):
+            print "argv[i]:", argv[i]
+            print "os.path.expanduser(argv[i]):", os.path.expanduser(argv[i])
+            print "os.path.expandvars(os.path.expanduser(argv[i])):", os.path.expandvars(os.path.expanduser(argv[i]))
+            print "os.path.abspath(os.path.expandvars(os.path.expanduser(argv[i]))):", os.path.abspath(os.path.expandvars(os.path.expanduser(argv[i])))
+            expanded_list.append(os.path.abspath(os.path.expandvars(os.path.expanduser(argv[i]))))
+        return expanded_list
+
+    def __init__(self, era="2016"):
+        self.parseArgs()
+        self.currentDirectory = os.getcwd() + "/"
+
+        self.move = None
+        self.move_object = None
+        self.move_subdirectories_at_dest = ""
+        self.moves = self.expandPath(*self.args.move)
+        if len(self.moves) == 0:
+            print "__init__: Can't move nothing"
+            exit(1)
+        self.move_isdir = False
+
+        self.common_locations["home"] = os.path.expanduser("~") + "/"
+        self.dprint("home:", self.common_locations["home"])
+        self.move_from_location = None
+
+        if self.args.user == "":
+            self.login = subprocess.check_output(["whoami"])[:-1]  # , "-p"
+            self.dprint("login from whoami:", self.login)
+        else:
+            self.login = self.args.user
+            self.dprint("login:", self.login)
+        for key in self.common_locations.keys():
+            self.common_locations[key] = self.getStandartizeDirectory(self.common_locations[key] + self.login)
+
+        self.to = self.args.to
+        if self.args.to_private is not None:
+            # Expand the system variables
+            self.args.to_private = self.expandPath(self.args.to_private)[0]
+            if self.args.to_private[0] == ".":
+                self.args.to_private = self.currentDirectory + self.args.to_private[2:]
+            self.dprint("Expanded final base-directory under which the corresponding sub-rectory wrt the user-base will be created:", self.args.to_private)
+
+            # Check the existance of the final destination
+            if not self.checkDirExists(path=self.args.to_private, critical=False):
+                if self.args.yes_on_command or self.yesORno("The final base-directory does not yet exist. Would you like to create it?"):
+                    bashCommand = "mkdir --parents " + self.args.to_private
+                    if not self.args.dry:
+                        print "Executing:", bashCommand
+                        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+                        output, error = process.communicate()
+                        self.dprint("\tmkdir::output:", output)
+                        if error is not None:
+                            print "\tmkdir::error:", error
+                            exit(1)
+                    else:
+                        self.dprint("# Would call command:", bashCommand)
+            # self.common_locations["to_private"] = self.getStandartizeDirectory(self.args.to_private)  # self.common_locations = {     "to_private": self.getStandartizeDirectory(self.args.to_private), }
+            # self.to = "to_private"
+            # Save the final destination base-directory
+            self.destination_root_path = self.getStandartizeDirectory(self.getStandartizeDirectory(self.args.to_private))
+            self.dpprint(self.common_locations)
+
+        else:
+            if self.to is not None:
+                if self.to not in self.common_locations.keys():
+                    print "Reffering to unrecognized destination location -> implement it"
+                    exit(1)
+                self.common_locations[self.to] = self.getStandartizeDirectory(self.common_locations[self.to] + self.login)
+                self.checkDirExists(path=self.common_locations[self.to], critical=True)
+                self.destination_root_path = self.getStandartizeDirectory(self.common_locations[self.to])
+
+            elif self.to is None:
+                print "No final destination was recognised."
+                exit(1)
+
+        # self.destination_root_path = self.getStandartizeDirectory(self.common_locations[self.to])
+        self.checkDirExists(path=self.destination_root_path, critical=True)
+        self.dprint("Destination root-path:", self.destination_root_path)
+
+        self.dprint("Current directory:", self.currentDirectory)
+
+    '''
     def __init__(self, era="2016"):
         self.parseArgs()
 
@@ -56,21 +145,25 @@ class MoveToNFS(object):
 
         self.currentDirectory = os.getcwd() + "/"
         self.dprint("Current directory:", self.currentDirectory)
+    '''
 
     def parseArgs(self):
         parser = argparse.ArgumentParser(description='movetoNFS.py parser')
-        parser.add_argument('--move', '-m', default="",
+        parser.add_argument('--move', '-m', default=[], nargs="*",
                             help='WHAT to move')
-        parser.add_argument('--to', '-t', default="",
+        parser.add_argument('--to', '-t', default=None, type=str,
                             help='WHERE to move')
-        parser.add_argument('--to-private', default="",
+        parser.add_argument('--to-private', default=None, type=str,
                             help='WHERE to move - private path, Example: /nfs/dust/cms/user/SOMENAME/afs/ ')
         parser.add_argument('--user', default="",
-                            help='user name')
+                            help='user name that should coinside with the user name in the base directory from where the subdirectory structure will be constructed')
         parser.add_argument('--dry', action='store_true', default=False,
                             help='dry run')
         parser.add_argument('--debug', action='store_true', default=False,
                             help='debug')
+        parser.add_argument('--yes-on-command', action='store_true', default=False,
+                            help='Do not ask before the command execution if there are no issues')
+
         self.args = parser.parse_args()
         self.dpprint("Parsed arguments:", self.args.__dict__)
 
@@ -105,18 +198,17 @@ class MoveToNFS(object):
     def locateMovedFile(self):
         '''self.move - the file that will be moved'''
         '''self.move_from_location - the initial generalised location, like home'''
-        '''self.move_subdirectories_at_dest - subdir structure to be created at the destination location where the files will be moved'''
+        '''self.move_subdirectories_at_dest - subdir structure to be created at the destination'''
         '''self.move_isdir'''
         '''self.move_object'''
 
         # Constract an global path of moved object
-        self.move = os.path.abspath(self.move_request)  # if self.move_request[0] != "/": self.currentDirectory + self.move_request
-        self.dprint("Apsolute path of moved object:", self.move)
+        self.dprint("Absolute path(s) of moved object(s):", self.move)
 
         if not os.path.exists(self.move):
             print "Moved object ... does not exist"
             exit(1)
-        
+
         self.move_object = os.path.basename(self.move)
         if os.path.isdir(self.move):
             self.move_isdir = True
@@ -124,39 +216,46 @@ class MoveToNFS(object):
             self.move_object = self.move.split("/")[-2]
         self.dprint("Moved object:", self.move_object)
 
-        # Determine what location is moving FROM; what subdir has to be made
-        print "==="
+        # Determine what subdirectory structure has to be created
+        # by comparing to the base directories defined in self.common_locations
         for key, location in self.common_locations.iteritems():
             if self.move[:len(location)] == location:
+                self.dprint(self.move[:len(location)], "==", location, "\n\tkey, location:", key, location)
                 self.move_from_location = key
-
                 self.move_subdirectories_at_dest = self.move[len(location):]
                 split_subdirs = self.move_subdirectories_at_dest.split("/")
                 # print "os.path.basename:", os.path.basename(self.move[len(location):])
                 # self.dprint("split_subdirs:", split_subdirs)
-                if self.move_isdir: self.move_subdirectories_at_dest = "/".join(split_subdirs[:-2])
-                else: self.move_subdirectories_at_dest = "/".join(split_subdirs[:-1])
+                if self.move_isdir:
+                    self.move_subdirectories_at_dest = "/".join(split_subdirs[:-2])
+                else:
+                    self.move_subdirectories_at_dest = "/".join(split_subdirs[:-1])
                 self.move_subdirectories_at_dest = self.getStandartizeDirectory(self.move_subdirectories_at_dest)
-
                 break
+            else:
+                self.dprint(self.move[:len(location)], "=/=", location,
+                    "\n\tkey, location:", key, location, "are not known among common")
 
         if self.move_from_location is None:
-            print "Could not estimate initial location of the object"
+            print "Could not estimate initial location of the object - you might want to add it to the known ones"
             exit(1)
 
         self.dprint("self.move_from_location:", self.move_from_location)
         self.dprint("self.move_subdirectories_at_dest:", self.move_subdirectories_at_dest)
 
-    def locateDestination(self):
-        self.destination = self.getStandartizeDirectory(self.destination_root_path + self.move_subdirectories_at_dest)
-        target = self.destination + self.move_object
+    def locateDestination(self, move_object=None, move_subdirectories_at_dest=""):
+        if move_object is None:
+            print "locateDestination: Can't move nothing"
+            exit(1)
+        self.destination = self.getStandartizeDirectory(self.destination_root_path + move_subdirectories_at_dest)
+        target = self.destination + move_object
         self.dprint("The target output:", target)
 
         if not self.checkDirExists(self.destination, critical=False):
             bashCommand = "mkdir --parents " + self.destination
             if not self.args.dry:
                 print "Executing:", bashCommand
-                if self.yesORno("Start execution?"):
+                if self.args.yes_on_command or self.yesORno("Start execution?"):
                     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
                     output, error = process.communicate()
                     self.dprint("\tmkdir::output:", output)
@@ -169,13 +268,13 @@ class MoveToNFS(object):
                 print "# Would call command:", bashCommand
         else:
             if os.path.exists(target):
-                if self.yesORno( " ".join(["Target", target, "ALREADY EXISTS!!!!! Cancel the execution of the script?"])):
-                    exut(1)
+                if self.yesORno(" ".join(["Target", target, "ALREADY EXISTS!!! Cancel the execution of the script?"])):
+                    exit(1)
             else:
                 self.dprint("Target output creates no conflicts")
-              
+
     def yesORno(self, question):
-        reply = str(raw_input(question+' (y/n): ')).lower().strip()
+        reply = str(raw_input(question + ' (y/n): ')).lower().strip()
         if len(reply) == 0:
             return self.yesORno("Uhhhh... please enter explicitle")
         elif reply[0].lower() == 'y':
@@ -186,28 +285,30 @@ class MoveToNFS(object):
             return self.yesORno("Uhhhh... please enter ")
 
     def run(self):
-        print "Hello, world!"
+        print "Run!"
+        for self.move in self.moves:
+            self.locateMovedFile()
+            self.locateDestination(move_object=self.move_object, move_subdirectories_at_dest=self.move_subdirectories_at_dest)
+            bashCommand = ""
 
-        self.locateMovedFile()
-        self.locateDestination()
-        bashCommand = ""
+            if len(self.move) > 0:
+                bashCommand = "mv " + self.move + " " + self.destination
 
-        if len(self.move) > 0:
-            bashCommand = "mv " + self.move + " " + self.destination
-
-        if not self.args.dry:
-            print "Executing:", bashCommand
-            if self.yesORno("Start execution?"):
-                process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-                output, error = process.communicate()
-                if len(output) > 0: self.dprint("\trun::output:", output)
-                if error is not None:
-                    print "\tmkdir::error:", error
-                    exit(1)
+            if not self.args.dry:
+                print "Executing:", bashCommand
+                if self.args.yes_on_command or self.yesORno("Start execution?"):
+                    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+                    output, error = process.communicate()
+                    if len(output) > 0:
+                        self.dprint("\trun::output:", output)
+                    if error is not None:
+                        print "\tmkdir::error:", error
+                        exit(1)
+                else:
+                    print "Executing declined"
             else:
-                print "Executing declined"
-        else:
-            print "# Would call command:", bashCommand
+                print "# Would call command:", bashCommand
+
 
 if __name__ == '__main__':
     MoveToNFS().run()
