2c7e26d371e22e6223286bb2135f0fd714451bd6
Olena Hlushchenko <greyxray@gmail.com>
2018-12-10 17:31:46 +0100

    olena:from naf

--- users/glusheno.sh
+++ users/glusheno.sh
@@ -18,12 +18,13 @@ ssh-add  ~/.ssh/id_rsa_nopass
 
 #DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 #cd $0
-DIR_PRIVATESETTINGS=${HOME}/RWTH/KIT/privatesettings
 DIR_BASH=${HOME}/RWTH/bashrc
 DIR_BASHHISTORY=/nfs/dust/cms/user/glusheno/bash_history
 SERVERBASH=${HOME}/RWTH/bashrc/users/glusheno.sh
 COMMONBASH=${HOME}/RWTH/bashrc/users/hlushchenko_common.sh  # ~ tilda is not expanded in scripts https://stackoverflow.com/a/3963747/3152072
-source ${DIR_BASH}/git-prompt.sh
+
+DIR_PRIVATESETTINGS=${HOME}/RWTH/KIT/privatesettings
+source ${DIR_PRIVATESETTINGS}/env_scripts/git-prompt.sh
 export HARRY_REMOTE_USER='ohlushch'
 # Path contains only pathes to the scripts
 export PATH="$HOME/.local/bin:$PATH"
@@ -33,8 +34,10 @@ export PATH=$DIR_PRIVATESETTINGS/playground:$PATH
 # Path contains only pathes to MODULES with __init__ defined
 [[ ":$PYTHONPATH:" != *"$HOME/.local/lib/python2.7/site-packages:"* ]] && PYTHONPATH="$HOME/.local/lib/python2.7/site-packages:${PYTHONPATH}"
 [[ ":$PYTHONPATH:" != *"$DIR_PRIVATESETTINGS:"* ]] && PYTHONPATH="$DIR_PRIVATESETTINGS:${PYTHONPATH}"
+
 source $COMMONBASH
 export PYTHONPATH
+
 # History
 mkdir -p $DIR_BASHHISTORY
 export HISTTIMEFORMAT="%F %T: "
